8b4abb2ab46adb78bbb7374dd9822d8d59cb45c2
unknown <amirsouayah@gmail.com>
2019-10-09 23:19:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Faq from './Faq';
+
+import Container from '@material-ui/core/Container';
+import Form from './Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <Container maxWidth="xl">
+          <Container maxWidth="lg">
+            <Faq/>
+            <Form/>
+          </Container>
+        </Container>
     </div>
   );
 }
---
+++ src/Faq.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import HideAppBar from './Hide';
+import BackToTop from './Scroltotop'
+import './Faq.css';
+
+
+const Faq = () => 
+
+<body>
+    <div className="division1">
+        <div></div>
+        <h3 className="faq">FAQ</h3><hr className="line"/>
+        <BackToTop/>
+    </div>
+</body>
+
+export default Faq 
